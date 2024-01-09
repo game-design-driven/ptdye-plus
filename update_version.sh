b9678609833597a7b3da8ee32c87c4f0ea333d09
Jasper Alani <jasperalani@gmail.com>
2024-01-09 03:26:46 +0000

    dev: add update version script

---
+++ update_version.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+unformattedVersion=$(awk -v FS="version = " 'NF>1{print $2}' build.gradle)
+
+unformattedVersion="${unformattedVersion:1}"
+version="${unformattedVersion::-14}"
+
+minor="${version: -1}"
+minorRemoved="${version::-1}"
+
+minorIncreased="$(($minor + 1))"
+
+newVersion="$minorRemoved$minorIncreased"
+
+# Replace in build.gradle
+find build.gradle -type f -exec sed -i "s/$version/$newVersion/g" {} \;
+
+# Replace in mods.toml
+find ./src/main/resources/META-INF/mods.toml -type f -exec sed -i "s/$version/$newVersion/g" {} \;
