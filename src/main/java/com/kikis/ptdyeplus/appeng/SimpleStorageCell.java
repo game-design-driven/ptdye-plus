a4a9cda9a0093953c1c42a792a00eb3fa37d71fa
CoolandonRS <49355953+CoolandonRS@users.noreply.github.com>
2024-01-15 02:56:15 -0700

    AE2Things and DISKs

--- src/main/java/com/kikis/ptdyeplus/appeng/SimpleStorageCell.java
+++ src/main/java/com/kikis/ptdyeplus/appeng/SimpleStorageCell.java
@@ -1,60 +1,31 @@
 package com.kikis.ptdyeplus.appeng;
 
-import appeng.api.config.FuzzyMode;
 import appeng.api.stacks.AEKeyType;
 import appeng.api.storage.cells.IBasicCellItem;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 
-public class SimpleStorageCell extends Item implements IBasicCellItem {
-    protected final double powerCost;
-    protected final int capacity;
-    protected final int typeCapacity;
-    protected final int costPerType;
-    private final AEKeyType keyType;
+public class SimpleStorageCell extends SimpleCellItem implements IBasicCellItem {
+    protected final int types;
+    protected final int bytesPerType;
 
     @Override
-    public AEKeyType getKeyType() { return keyType; }
+    public AEKeyType getKeyType() { return _getKeyType(); }
 
     @Override
-    public int getBytes(ItemStack itemStack) { return this.capacity; }
+    public int getBytes(ItemStack is) { return _getBytes(is); }
 
     @Override
-    public int getBytesPerType(ItemStack itemStack) { return costPerType; }
+    public int getBytesPerType(ItemStack is) { return bytesPerType; }
 
     @Override
-    public int getTotalTypes(ItemStack itemStack) { return typeCapacity; }
+    public int getTotalTypes(ItemStack cellItem) { return types; }
 
     @Override
-    public double getIdleDrain() { return powerCost; }
-
-    @Override
-    public FuzzyMode getFuzzyMode(ItemStack itemStack) {
-        // clone of: https://github.com/AppliedEnergistics/Applied-Energistics-2/blob/137cb538538b7ef9c66261931f662182ddf937b4/src/main/java/appeng/items/storage/BasicStorageCell.java#L133
-        final String fz = itemStack.getOrCreateTag().getString("FuzzyMode");
-        if (fz.isEmpty()) {
-            return FuzzyMode.IGNORE_ALL;
-        }
-        try {
-            return FuzzyMode.valueOf(fz);
-        } catch (Throwable t) {
-            return FuzzyMode.IGNORE_ALL;
-        }
-    }
-
-    @Override
-    public void setFuzzyMode(ItemStack itemStack, FuzzyMode fuzzyMode) {
-        // clone of: https://github.com/AppliedEnergistics/Applied-Energistics-2/blob/137cb538538b7ef9c66261931f662182ddf937b4/src/main/java/appeng/items/storage/BasicStorageCell.java#L145
-        itemStack.getOrCreateTag().putString("FuzzyMode", fuzzyMode.name());
-    }
+    public double getIdleDrain() { return _getIdleDrain(); }
 
     public SimpleStorageCell(Properties properties, double drain, int bytes, int types, int bytesPerType, AEKeyType keyType) {
-        super(properties);
-        if (bytes < 8 || bytes % 8 != 0) throw new IllegalArgumentException("Bytes must be a positive non-zero multiple of 8");
-        this.powerCost = drain;
-        this.capacity = bytes;
-        this.typeCapacity = types;
-        this.costPerType = bytesPerType;
-        this.keyType = keyType;
+        super(properties, drain, bytes, keyType);
+        this.types = types;
+        this.bytesPerType = bytesPerType;
     }
 }
--- src/main/java/com/kikis/ptdyeplus/kubejs/bindings/AppengBindings.java
+++ src/main/java/com/kikis/ptdyeplus/kubejs/bindings/AppengBindings.java
@@ -2,5 +2,5 @@ package com.kikis.ptdyeplus.kubejs.bindings;
 
 @SuppressWarnings("unused")
 public final class AppengBindings {
-    DiskUtilJS disks = new DiskUtilJS();
+    CellUtilJS cells = new CellUtilJS();
 }
