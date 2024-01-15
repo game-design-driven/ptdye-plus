93dc20462addd61928ee3787185ead82dc4f4de6
Jasper Alani <jasperalani@gmail.com>
2024-01-15 00:40:44 +0000

    Update update_version.sh

--- update_version.sh
+++ update_version.sh
@@ -5,12 +5,11 @@ unformattedVersion=$(awk -v FS="version = " 'NF>1{print $2}' build.gradle)
 unformattedVersion="${unformattedVersion:1}"
 version="${unformattedVersion::-14}"
 
-minor="${version: -1}"
-minorRemoved="${version::-1}"
-
-minorIncreased="$(($minor + 1))"
-
-newVersion="$minorRemoved$minorIncreased"
+#minor="${version: -1}"
+#minorRemoved="${version::-1}"
+#minorIncreased="$(($minor + 1))"
+#newVersion="$minorRemoved$minorIncreased"
+newVersion="$1"
 
 # Replace in build.gradle
 find build.gradle -type f -exec sed -i "s/$version/$newVersion/g" {} \;
