c102059a85ac26d0d21df9a85bbb9711f62e8888
EngineerSmith <56016593+EngineerSmith@users.noreply.github.com>
2026-02-04 17:45:38 +0000

    feat: added invisible seat block

---
+++ src/main/java/com/gdd/ptdyeplus/content/block/InvisibleSeatBlock.java
@@ -0,0 +1,42 @@
+package com.gdd.ptdyeplus.content.block;
+
+import com.simibubi.create.content.contraptions.actors.seat.SeatBlock;
+import net.minecraft.MethodsReturnNonnullByDefault;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.DyeColor;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+public class InvisibleSeatBlock extends SeatBlock {
+    public InvisibleSeatBlock(Properties properties) {
+        super(properties, DyeColor.RED);
+    }
+
+    @Override
+    public RenderShape getRenderShape(BlockState state) {
+        return RenderShape.INVISIBLE;
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return Shapes.block();
+    }
+
+    @Override
+    public float getShadeBrightness(BlockState state, BlockGetter level, BlockPos pos) {
+        return 1.0f;
+    }
+
+    @Override
+    public boolean propagatesSkylightDown(BlockState state, BlockGetter level, BlockPos pos) {
+        return true;
+    }
+}
