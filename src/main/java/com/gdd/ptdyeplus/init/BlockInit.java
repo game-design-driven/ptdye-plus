79ab69c746872826a088550a6c0b26f8fb4afd2f
EngineerSmith <56016593+EngineerSmith@users.noreply.github.com>
2026-02-04 17:44:06 +0000

    feat: added Thruster Block

--- src/main/java/com/gdd/ptdyeplus/init/BlockInit.java
+++ src/main/java/com/gdd/ptdyeplus/init/BlockInit.java
@@ -1,10 +1,28 @@
 package com.gdd.ptdyeplus.init;
 
 import com.gdd.ptdyeplus.PTDyePlus;
+import com.gdd.ptdyeplus.content.block.InvisibleSeatBlock;
+import com.gdd.ptdyeplus.content.block.ThrusterBlock;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class BlockInit {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, PTDyePlus.ID);
+
+    public static final RegistryObject<Block> THRUSTER = BLOCKS.register("thruster",
+        () -> new ThrusterBlock(BlockBehaviour.Properties.of()
+            .strength(5f)
+            .noOcclusion()
+            .dynamicShape()
+        ));
+
+    public static final RegistryObject<Block> INVISIBLE_SEAT = BLOCKS.register("invisible_seat",
+        () -> new InvisibleSeatBlock(BlockBehaviour.Properties.of()
+            .strength(5f)
+            .noOcclusion()
+            .isValidSpawn((a, b, c, d) -> false)
+        ));
 }
--- src/main/java/com/gdd/ptdyeplus/init/ItemInit.java
+++ src/main/java/com/gdd/ptdyeplus/init/ItemInit.java
@@ -1,10 +1,18 @@
 package com.gdd.ptdyeplus.init;
 
 import com.gdd.ptdyeplus.PTDyePlus;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ItemInit {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, PTDyePlus.ID);
+
+    public static final RegistryObject<Item> THRUSTER = ITEMS.register("thruster",
+        () -> new BlockItem(BlockInit.THRUSTER.get(), new Item.Properties()));
+
+    public static final RegistryObject<Item> INVISIBLE_SEAT = ITEMS.register("invisible_seat",
+        () -> new BlockItem(BlockInit.INVISIBLE_SEAT.get(), new Item.Properties()));
 }
