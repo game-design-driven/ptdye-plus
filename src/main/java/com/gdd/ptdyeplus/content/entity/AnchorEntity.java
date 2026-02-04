6a3fa333b459bfd4542c12e6a91ff3ee112d9077
EngineerSmith <56016593+EngineerSmith@users.noreply.github.com>
2026-02-04 17:49:57 +0000

    refactor: added freeze property to anchor entity to hold it in place during animations

--- src/main/java/com/gdd/ptdyeplus/content/entity/AnchorEntity.java
+++ src/main/java/com/gdd/ptdyeplus/content/entity/AnchorEntity.java
@@ -1,18 +1,41 @@
 package com.gdd.ptdyeplus.content.entity;
 
+import net.createmod.catnip.math.VecHelper;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.network.syncher.EntityDataSerializers;
+import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MoverType;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
 
 public class AnchorEntity extends Entity {
+
+    private static final EntityDataAccessor<Boolean> FROZEN = SynchedEntityData.defineId(AnchorEntity.class, EntityDataSerializers.BOOLEAN);
+
+    private Vec3 frozenPosition = Vec3.ZERO;
 
     public AnchorEntity(EntityType<?> type, Level level) {
         super(type, level);
         this.noPhysics = false;
         this.blocksBuilding = false;
+    }
+
+    public void freeze() {
+        this.frozenPosition = new Vec3(getX(), getY(), getZ());
+        this.entityData.set(FROZEN, true);
+        this.setDeltaMovement(Vec3.ZERO);
+    }
+
+    public void unfreeze() {
+        this.entityData.set(FROZEN, false);
+    }
+
+    public boolean isFrozen() {
+        return this.entityData.get(FROZEN);
     }
 
     @Override
@@ -31,11 +54,32 @@ public class AnchorEntity extends Entity {
     public void tick() {
         super.tick();
 
-        move(MoverType.SELF, getDeltaMovement());
+        if (isFrozen()) {
+            setDeltaMovement(Vec3.ZERO);
+            if (!level().isClientSide) {
+                double distSq = this.position().distanceToSqr(frozenPosition);
+                if (distSq > 1e-4) {
+                    teleportTo(frozenPosition.x, frozenPosition.y, frozenPosition.z);
+                }
+            }
+        } else {
+            move(MoverType.SELF, getDeltaMovement());
 
-        if (!level().isClientSide) {
-            checkInsideBlocks();
+            if (!level().isClientSide) {
+                checkInsideBlocks();
+            }
         }
+    }
+
+    @Override
+    public void lerpTo(double x, double y, double z, float yRot, float xRot, int steps, boolean teleport) {
+        if (isFrozen()) {
+            super.lerpTo(x, y, z, yRot, xRot, 0, true);
+            this.setPos(x, y, z);
+            this.setRot(yRot, xRot);
+            return;
+        }
+        super.lerpTo(x, y, z, yRot, xRot, steps, teleport);
     }
 
     @Override
@@ -49,11 +93,43 @@ public class AnchorEntity extends Entity {
     }
 
     @Override
-    protected void defineSynchedData() {}
+    protected void defineSynchedData() {
+        this.entityData.define(FROZEN, false);
+    }
 
     @Override
-    protected void readAdditionalSaveData(CompoundTag nbt) {}
+    protected void readAdditionalSaveData(CompoundTag nbt) {
+        boolean isFrozen = nbt.getBoolean("IsFrozen");
+        this.entityData.set(FROZEN, isFrozen);
+        if (isFrozen) {
+            frozenPosition = VecHelper.readNBT(nbt.getList("FrozenPosition", CompoundTag.TAG_DOUBLE));
+        }
+    }
 
     @Override
-    protected void addAdditionalSaveData(CompoundTag nbt) {}
+    protected void addAdditionalSaveData(CompoundTag nbt) {
+        boolean isFrozen = isFrozen();
+        nbt.putBoolean("IsFrozen", isFrozen);
+        if (isFrozen) {
+            nbt.put("FrozenPosition", VecHelper.writeNBT(frozenPosition));
+        }
+    }
+
+    @Override
+    public boolean canBeCollidedWith() {
+        return true;
+    }
+
+    @Override
+    public boolean isPushable() {
+        return false;
+    }
+
+    @Override
+    public void push(Entity entity) {}
+
+    @Override
+    public boolean canCollideWith(Entity entity) {
+        return super.canCollideWith(entity);
+    }
 }
